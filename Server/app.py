87c451a9e706c52dd70fc23524509d2d4b123791
Omar Atef <oatef98@gmail.com>
2022-10-20 18:47:32 +0200

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,8 @@
-import json
 from Server import app
 from flask import (request, jsonify)
 
-from Server.Model.OLX import Ads
-from Server.Service.OLX_Service import (create_olx_post, get_all_posts, get_with_limit)
-from Server.Service.Scraper_service import scrape_data, send_mail
+from Server.Service.OLX_Service import (get_all_posts)
+from Server.Service.Scraper_service import scrape_data
 
 
 @app.route('/scrape_olx', methods=['POST'])
